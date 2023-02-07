52fa6b3fe6294f676295bd64e9a1a4e19b143b72
Jesus Antonio Garcia Fusi <83846459+SilentCry1234@users.noreply.github.com>
2023-02-07 18:10:30 -0300

    Update ThirdScript.cs
    Se termino el tuto

--- Assets/2.Scripts/ThirdScript.cs
+++ Assets/2.Scripts/ThirdScript.cs
@@ -4,25 +4,57 @@ using UnityEngine;
 
 public class ThirdScript : MonoBehaviour
 {
-
+    int numero;
+    float numeroFloat; 
 
     void Start()
     {
+        Enemy enemy1 = new Enemy();
+
+        enemy1.vida = 5;
+        
+        enemy1.MeleeAttack();
+
+        /////////
+
+        FlyingEnemy flyingEnemy1= new FlyingEnemy();
+        
+        flyingEnemy1.FlyingAttack();
+
+        //PlayerPrefs.SetInt("NumeroInt", 4);
+        //PlayerPrefs.SetFloat("NumeroFloat", 7.5f);
+        
         //StartCoroutine(Coroutine(3.5f)); 
-        Invoke("Disparar", 3); 
+        //Invoke("Disparar", 3);
+        
+        //numero = PlayerPrefs.GetInt("NumeroInt");
+        //numeroFloat = PlayerPrefs.GetFloat("NumeroFloat"); 
     }
 
     private void Update()
     {
 
-
+        //Debug.Log(numero);
+        //Debug.Log(numeroFloat);
 
     }
-
-    void Disparar()
+    void GuardarInfo()
     {
-        Debug.Log("Disparo"); 
+        // vidas, puntos, nombres de skins
+        //PlayerPrefs.Set...
     }
+
+    void CargarInfo()
+    {
+        //Carga las cosas
+        //... = PlayerPrefs.Get...
+        
+    }
+
+    //void Disparar()
+    //{
+    //    Debug.Log("Disparo"); 
+    //}
 
 
     //IEnumerator Coroutine (float time)
@@ -41,4 +73,26 @@ public class ThirdScript : MonoBehaviour
     //    StartCoroutine(Coroutine(3));
 
     //}
+
+    class Enemy
+    {
+        public int vida;
+
+        public float attack;
+
+        public string nombre;
+
+        public void MeleeAttack() 
+        {
+            Debug.Log("MeleeAttack"); 
+        }
+    }
+
+    class FlyingEnemy : Enemy
+    {
+        public void FlyingAttack()
+        {
+            Debug.Log("FLyingAttack");
+        }
+    }
 }
